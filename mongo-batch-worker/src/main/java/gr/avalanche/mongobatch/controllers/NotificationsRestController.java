b55d3e132fb6fb69a42536bd8041ec1b17b4891a
npapadopoulos <npapadopoulos@cognity.gr>
2017-07-17 15:24:18 +0300

    mongo-batch-db-mongo Module

--- mongo-batch-worker/src/main/java/gr/avalanche/mongobatch/controllers/NotificationsRestController.java
+++ mongo-batch-worker/src/main/java/gr/avalanche/mongobatch/controllers/NotificationsRestController.java
@@ -1,5 +1,7 @@
 package gr.avalanche.mongobatch.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,8 @@ import java.sql.Timestamp;
 @RestController
 public class NotificationsRestController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(NotificationsRestController.class);
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
@@ -21,9 +25,10 @@ public class NotificationsRestController {
     public String hello() { //TODO Delete This Method
         Timestamp persisted = null;
         try {
+            LOGGER.info("hello() Method Invoked.");
             persisted = jdbcTemplate.queryForObject("SELECT testing FROM testing WHERE id = 1", Timestamp.class);
-        } catch (Exception e) {
-            //DO Nothing
+        } catch (Exception exception) {
+            LOGGER.error("Exception", exception);
         }
         return persisted==null ? "No Entries Found" : "Persisted Value Is " + persisted;
     }
